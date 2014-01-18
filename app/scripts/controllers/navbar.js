055d3147639a43f1e286cfbe129076c50b75c129
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-01-18 11:52:36 +0100

    Modifying the controller to add the management.

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('exampleApp')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+      return (viewLocation === $location.path());
+    };
   });
