40a09755bc5c90bc7ea592f7e611cd71c3d2945b
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-01-19 09:16:26 +0100

    Modifying the test configuration

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
     exclude: [],
 
     // web server port
-    port: 8080,
+    port: 9876,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
@@ -35,7 +35,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
